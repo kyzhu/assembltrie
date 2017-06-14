4c147c4d54acc8942d5c5451a89f26f2461efbb7
kzhu <kzhu@indiana.edu>
2017-06-13 22:55:52 -0400

    Update error.hpp

--- error.hpp
+++ error.hpp
@@ -4,14 +4,23 @@
 class Error {
 public:
 	int location;
+	char underlying;
 	char base;
 
 	Error(int location, char base) {
 		this->location = location;
 		this->base = base;
+		this->underlying = 'X';
+	}
+
+	Error(int location, char base, char underlying) {
+		this->location = location;
+		this->base = base;
+		this->underlying = underlying;
 	}
 
 	~Error(){};
 };
 
 #endif
+
