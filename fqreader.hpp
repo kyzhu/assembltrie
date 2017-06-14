001a87ee2b9984f3c381c46b152b0b666eb272c2
kzhu <kzhu@indiana.edu>
2017-06-13 23:02:10 -0400

    Update fqreader.hpp

--- fqreader.hpp
+++ fqreader.hpp
@@ -1,4 +1,8 @@
+#ifndef FQREADER_HPP_
+#define FQREADER_HPP_
+
 #include <string>
+#include <experimental/string_view>
 #include <vector>
 #include <pthread.h>
 
@@ -12,45 +16,76 @@ class FastaReader {
 private:
 	/* Parameters */
 	static int L;
+	static int K;
 	static int MAX_ERRORS;
 
+	/* Heuristic to correct read orientation */
+	static int heuristic;
+
 public:
+	bool print_info = false;
+
+	/* If the suffix length of a read is less than S, then it will not be considered in addChildren. */
+	int S;
+
+	/* Whether post-process. */
+	int POST_PROCESS = 0;
+
 	/* Input and output file names */
 	std::string INFILE;
 	std::string OUTFILE;
 
 	/* Shared variables */
-	//Hash starts, ends;
+	Hash *starts;
+	Hash *ends;
 	tbb::concurrent_vector<Read*> reads;
 	int sequences = 0;
-	int read_id = 0;
+	int current_bucket = 0;
+	std::vector<std::experimental::string_view> htcReads;
+	std::vector<int> htcBits;
+
+	int test = 0;
 
 	/* Thread control */
-	//pthread_mutex_t if_mutex;
-	//pthread_spinlock_t read_access;
 	int num_threads = 1;
 	pthread_mutex_t thread_lock;
-	bool *processed;
+	pthread_spinlock_t read_access;
 	std::vector<std::string> bases_unsplit;
-	std::vector<std::vector<std::string>> bases_split; 
+	std::vector<std::vector<std::string>> bases_split;
+	long split_size;
+	std::vector<int> rc_indicator;
 
-	FastaReader(int, int, const std::string &ifn, const std::string &ofn);
+	FastaReader(int, int, int, int, const std::string &ifn,
+			const std::string &ofn, int, int);
 	~FastaReader() {
-		//for (size_t i = 0; i < reads.size(); i++)
-		//	delete reads[i];
+		delete starts; 
+		delete ends;
+		for (size_t i = 0; i < reads.size(); i++)
+			if (reads[i])
+				delete reads[i];
 	}
 
+	/* For encoder. */
 	static int getL() {
 		return L;
 	}
 
+	/* Read in fastq files. */
 	void readFastaq(int);
+	void readFastaqRC(int);
 
 	/* Returns a new read with bases BASES and correct parent, or null if already exists. */
-	Read* addRead(std::string, Hash*);
+	Read* addRead(std::experimental::string_view);
+	Read* addRead(std::experimental::string_view, int);
 
 	/* Attaches appropriate children to a newly-created read. */
-	void addChildren(Read*, std::string, Hash*);
+	void addChildren(Read*, std::experimental::string_view);
+
+	/* Postprocess reads such that the width of tries is reduced.*/
+	static bool suff_order(Read *r, Read *s) {
+		return (r->suffix > s->suffix);
+	}
+	void postProcess(Read*);
 
 	/* Adds a read to a sequence if not already in one, and dumps sequence to file. */
 	void addSequence(Read*, BitWriter*);
@@ -58,20 +93,32 @@ public:
 	/* Returns base with the most votes. */
 	char vote(int*, int);
 
-	/* Dumps READS to file. */
-	void dumpReads(std::vector<Read*>, BitWriter*);
-
 	/* Returns the Hamming distance between two strings. */
-	int diff(std::string, std::string);
+	int diff(std::experimental::string_view, std::experimental::string_view);
 
 	/* Returns the current time (in millisecond) */
 	unsigned long currentTimeMillis();
 
 	/* Compress the fasta(q) file */
 	void *_thread();
-	static void *thread(void *obj) {
-		return ((FastaReader*)obj)->_thread();
-	}
-	void run(int);
+	void *_thread_orcom();
+	void *_thread_shortsuf();
 
+	static void *thread(void *obj) {
+		switch (heuristic) {
+		case 0:
+			return ((FastaReader*) obj)->_thread();
+		case 1:
+			return ((FastaReader*) obj)->_thread_orcom();
+		case 2:
+			return ((FastaReader*) obj)->_thread_shortsuf();
+		default:
+			return ((FastaReader*) obj)->_thread();
+		}
+	}
+
+	void run(int);
 };
+
+#endif
+
