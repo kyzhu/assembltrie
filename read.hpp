f06da8a427334e0c8aa05df607f4d2a38bebca93
kzhu <kzhu@indiana.edu>
2017-06-13 23:07:15 -0400

    Update read.hpp

--- read.hpp
+++ read.hpp
@@ -2,60 +2,76 @@
 #define READ_HPP_
 
 #include <vector>
-#include <string>
+#include <experimental/string_view>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "error.hpp"
 
 class Read{
 public:
+	/* Parent and children */
 	Read *parent = NULL;
-	std::string suffix;
-	std::string _bases;
+	std::vector<int> children;
 
-	//bool connected = false;
-	int ID = -1;
+	/* Bases */
+	std::experimental::string_view _bases;
+	int suffix;
+
+	/* Basic information */
+	int ID = -1; // ID
+	std::vector<int> rc;
 	int count = 1; // Number of times this read appeared in the input
-	int seq = -1; // Sequence containing this read, or -1 if none
+	int trieID = -1; // ID of the trie containing this read
+	int seq = -1; // ID of the sequence containing this read, or -1 if none
 	int seqId = -1; // ID in sequence, or -1 if none
-	int children = 0; // Number of children
+	bool merged = false; // For post-process
+
+	pthread_spinlock_t access;
+
+	/* Errors */
 	std::vector<Error> errors;
 
-	/* Constructor */
-	Read(std::string);
-	Read(std::string, std::string);
+	/* Constructors */
+	Read(std::experimental::string_view);
+	Read(std::experimental::string_view, int);
+	Read(std::experimental::string_view, int, int);
 	~Read(){
                 //if (parent != NULL) delete parent;
         }
 
+	int getSuffixlength();
+
 	/* Sets my parent to PARENT (with overlap LENGTH) and return myself. */
 	Read* connect(Read*, int);
 
-	/* Last L bases of read R. */
-	static std::string bases(Read *r, size_t l) {
-		return bases(r, l, true);
+	/* Disconnect a child read and return myself. */
+	Read* disconnect(Read*);
+
+	/* Test whether I am the ancestor of another read. */
+	bool isAncestor(Read*);
+
+	/* Test whether a read has children or not. */
+	bool hasChildren() {
+		return (!children.empty());
 	}
 
-	/* Last L bases of read R. */
-	static std::string bases(Read*, size_t, bool);
+	/* Get all bases. */
+	std::experimental::string_view bases();
 
-	/* Last L bases. */
-	std::string bases(size_t l) {
-		return bases(this, l);
+	/* Get last L bases. */
+	std::experimental::string_view bases(size_t l);
+
+	/* Get bases of length L from BEGIN_INDEX (inclusive) */
+	std::experimental::string_view bases(size_t begin_index, size_t l) {
+		return _bases.substr(begin_index, l);
 	}
 
-	/* Last L bases. */
-	std::string bases(size_t l, bool e) {
-		return bases(this, l, e);
-	}
-
-	/* All bases. */
-	std::string bases();
-
-	std::string toString() {
+	std::experimental::string_view toString() {
 		return bases();
 	}
 
 };
 
 #endif
+
